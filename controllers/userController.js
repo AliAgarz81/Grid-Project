4335021d85479e7a9337defc5c05a8b5b94ea49f
AliAgarz81 <aliagarzayev81@gmail.com>
2024-05-14 23:44:00 +0400

    final

--- controllers/userController.js
+++ controllers/userController.js
@@ -41,6 +41,7 @@ const loginUser = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: true,
+            sameSite: none,
             expires: new Date(Date.now() + monthInSeconds * 3000)
         }).send({ message: 'Login successfully' });
     } catch(error) {
