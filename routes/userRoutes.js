769852cc5379a46cda662efefcd5eae2c71e6e23
AliAgarz81 <aliagarzayev81@gmail.com>
2024-05-19 18:14:38 +0400

    Some sanitizing added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 const { createUser, loginUser, logoutUser, getName, checkUser, getUsers, deleteUser } = require('../controllers/userController');
 const authGuard = require('../middlewares/authGuard');
+const { body } = require('express-validator');
 
 const router = require('express').Router();
 
 router.post('/create', createUser);
-router.post('/login', loginUser);
+router.post('/login', body("name").escape(), body("password").escape(), loginUser);
 router.post('/logout', logoutUser);
 router.get('/', authGuard, getName);
 router.get('/all', authGuard, getUsers);
